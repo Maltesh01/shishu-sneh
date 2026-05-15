9731481fca45b758b57629753dc7d8b2bcb7e3c3
Maltesh01 <maltesh200119@gmail.com>
2026-05-15 22:53:57 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,1 +1,16 @@
 
+package com.shishusneh.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.shishusneh.app.ui.navigation.AppNavGraph
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppNavGraph()
+        }
+    }
+}
